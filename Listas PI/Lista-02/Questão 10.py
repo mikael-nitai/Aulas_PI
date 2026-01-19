0a26de4e3b3f2d477b64e75ac23fe6ee1dd85d5d
Mikael Nitai <mikaelnitaicderose@gmail.com>
2026-01-19 11:27:38 -0300

    Lista-03a:questão 2

--- Listas PI/Lista-02/Questão 10.py
+++ Listas PI/Lista-02/Questão 10.py
@@ -3,12 +3,12 @@ idade = int(input("Digite sua idade: "))
 resposta_domingo = input("Digite se a data atual é um domingo (S/N): ").lower()
 
 eh_domingo = resposta_domingo == "s"
-entrada_gratuita = idade < 12 and eh_domingo
+# entrada_gratuita = idade < 12 and eh_domingo
 
 # print("A entrada será gratuita" if entrada_gratuita else "Entrada gratuita não aprovada")
 if eh_domingo and idade < 12:
     print("A entrada será gratuita")
-elif idade >= 12:
+elif idade >= 12 and eh_domingo:
     print("Iddade além do limite da promoção.")
 else:
     print("A promoção só é válida nos domingos")
