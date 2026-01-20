a30fcfc3bf195368eb6a66e48e37c6a6ec024353
Mikael Nitai <mikaelnitaicderose@gmail.com>
2026-01-20 11:19:18 -0300

    quest 2

---
+++ Listas PI/Lista-função/Q2.py
@@ -0,0 +1,7 @@
+#2 - Faça uma função em Python que mostre o tamanho de uma String
+def tamanho(string):
+    return len(string)
+
+texto=input("Digite uma string válida: ")
+print(f" o tamanho da string é {tamanho(texto)} caracteres")
+
